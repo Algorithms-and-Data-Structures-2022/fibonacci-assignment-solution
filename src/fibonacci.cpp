e378408e53ffaf616a71722ce5c746dbf6d08275
Ramil Safin <RamilSafNab1996@gmail.com>
2022-04-14 16:02:06 +0300

    Update fibonacci.cpp

--- src/fibonacci.cpp
+++ src/fibonacci.cpp
@@ -11,9 +11,10 @@ namespace assignment {
       return n;
     }
 
-    // Напишите здесь свой код ...
+    const double sqrt_5 = std::sqrt(5);
+    const double fib = std::pow((1 + sqrt_5) * 0.5, n) / sqrt_5;
 
-    return 0;
+    return static_cast<int64_t>(std::round(fib));
   }
 
   int64_t fib_iterative(int n) {
@@ -22,20 +23,28 @@ namespace assignment {
       return n;
     }
 
-    // буфер для хранения двух последних значения ряда Фибоначчи
+    // buffer previous fibonacci numbers
     int64_t fib_prev = 0;
     int64_t fib_curr = 1;
 
-    // Напишите здесь свой код ...
+    int64_t fib = 0;
 
-    return 0;
+    for (int i = 1; i < n; ++i) {
+      fib = fib_prev + fib_curr;
+      fib_prev = fib_curr;
+      fib_curr = fib;
+    }
+
+    return fib;
   }
 
   int64_t fib_recursive(int n) {
 
-    // Напишите здесь свой код ...
+    if (n <= 1) {
+      return n;
+    }
 
-    return 0;
+    return fib_recursive(n - 1) + fib_recursive(n - 2);
   }
 
   int64_t fib_recursive_memoization(int n, std::vector<int64_t>& cache) {
@@ -45,13 +54,13 @@ namespace assignment {
     }
 
     if (cache[n] != -1) {
-      // Напишите здесь свой код ...
-      return 0;
+      return cache[n];
     }
 
-    // Напишите здесь свой код ...
+    // memoization
+    cache[n] = fib_recursive_memoization(n - 1, cache) + fib_recursive_memoization(n - 2, cache);
 
-    return 0;
+    return cache[n];
   }
 
   int64_t fib_matrix(int n) {
@@ -60,11 +69,9 @@ namespace assignment {
       return n;
     }
 
-    // Напишите здесь свой код ...
+    const Matrix2x2 res = matrix_power(FibMatrix, n - 1);
 
-    // Tip: используйте реализованную функцию matrix_pow
-
-    return 0;
+    return res[1][1];
   }
 
 }  // namespace assignment
